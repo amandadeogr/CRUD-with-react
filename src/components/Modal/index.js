0460eaf563f73f60108ba02d488e414734f0eca0
amandadeog <amandadeog@gmail.com>
2022-10-14 22:01:39 -0300

    add modal structure

---
+++ src/components/Modal/index.js
@@ -0,0 +1,22 @@
+import './Modal.css';
+
+const Modal = () => {
+   return (
+      <div className='modal'>
+         <div className='modal-content'>
+            <header>
+               <h2>Novo cliente</h2>
+            </header>
+
+            <form />
+
+            <footer>
+               <button />
+               <button />
+            </footer>
+         </div>
+      </div>
+   )
+}
+
+export default Modal;
