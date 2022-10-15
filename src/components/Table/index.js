ce4c8815486d0f0e604b184f976b78d024a159a9
amandadeog <amandadeog@gmail.com>
2022-10-14 23:19:31 -0300

    table creation

---
+++ src/components/Table/index.js
@@ -0,0 +1,21 @@
+import './Table.css';
+
+const Table = () => {
+   return (
+      <table id='table'>
+         <thead>
+            <tr>
+               <th>Nome</th>
+               <th>Email</th>
+               <th>Data de nascimento</th>
+               <th>Ações</th>
+            </tr>
+         </thead>
+         <tbody>
+
+         </tbody>
+      </table>
+   )
+}
+
+export default Table;
