b062c854da8b4f4cf32f55788806d50fa42d1ae0
amandadeog <amandadeog@gmail.com>
2022-10-14 23:20:12 -0300

    render Table component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Modal from './components/Modal';
+import Table from './components/Table';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Modal />
+      <Table />
     </div>
   );
 }
