78248a5c9eb7662209f670c377510370323fb37c
amandadeog <amandadeog@gmail.com>
2022-10-14 22:37:33 -0300

    render Modal component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Modal from './components/Modal';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Modal />
     </div>
   );
 }
